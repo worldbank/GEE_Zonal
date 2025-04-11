e23945639bcc41dcc5ac1547fa08d8a88c5fc97a
Andres Chamorro <achamorroelizond@worldbank.org>
2025-04-10 18:49:32 -0600

    add -v to init

--- src/gee_zonal/__init__.py
+++ src/gee_zonal/__init__.py
@@ -3,3 +3,5 @@ from .catalog import Catalog
 from .gee_helpers import gpd_to_gee, authenticateGoogleDrive
 
 __all__ = ["ZonalStats", "Catalog", "gpd_to_gee", "authenticateGoogleDrive"]
+__author__ = """Andres Chamorro"""
+__version__ = "0.1.0"
